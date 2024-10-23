cc487364a189616529738823caf663818f32d995
nihala pp <nihalapp00@gmail.com>
2024-10-23 13:29:28 +0530

    Fix deployment issues

--- mahallu_info_system/settings.py
+++ mahallu_info_system/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-8xsuoc3=#mgv9azae&t7!tdjk86j(=8c7niwbipz0!&yp=re$@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'mahallu.onrender.com']
+
 
 
 
